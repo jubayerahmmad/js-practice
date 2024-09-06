6fc811bcf80c34e411186260b1d9c78d1a105752
jubayerahmmad <zubayersami510@gmail.com>
2024-09-06 15:36:25 +0600

    practice

--- array.js
+++ array.js
@@ -1,35 +1,98 @@
-// task1
-const fruits = ["banana", "apple", "orange", "guava"];
-console.log(fruits[3]);
+//? task1
+// const fruits = ["banana", "apple", "orange", "guava"];
+// console.log(fruits[3]);
 
-fruits[2] = "jambura";
-console.log(fruits);
+// fruits[2] = "jambura";
+// console.log(fruits);
 
-// task2
-const touristSpot = ["koshkobajar", "rangamati", "sajek", "sylhet"];
-touristSpot.push("oshtrolia");
+//? task2
+// const touristSpot = ["koshkobajar", "rangamati", "sajek", "sylhet"];
+// touristSpot.push("oshtrolia");
 
-console.log(touristSpot);
-touristSpot.push("bandarban", "sundarban");
-console.log(touristSpot);
-touristSpot.pop();
-console.log(touristSpot);
+// console.log(touristSpot);
+// touristSpot.push("bandarban", "sundarban");
+// console.log(touristSpot);
+// touristSpot.pop();
+// console.log(touristSpot);
 
-// task3
-const books = ["bangla", "inreji", "goonit", "spanish"];
+//? task3
+// const books = ["bangla", "inreji", "goonit", "spanish"];
 
-console.log(books.includes("spanish"));
+// console.log(books.includes("spanish"));
 
-// task4
-const balsal = "ahg hsha yhuga";
+//? task4
+// const balsal = "ahg hsha yhuga";
 
-console.log(Array.isArray(books));
-console.log(Array.isArray(fruits));
-console.log(Array.isArray(touristSpot));
-console.log(Array.isArray(balsal));
+// console.log(Array.isArray(books));
+// console.log(Array.isArray(fruits));
+// console.log(Array.isArray(touristSpot));
+// console.log(Array.isArray(balsal));
 
-// task5
-const numbers = ["1", "2", "3", "4", "5"];
-const boooks = ["bangla", "inreji", "goonit"];
+//? task5
+// const numbers = ["1", "2", "3", "4", "5"];
+// const boooks = ["bangla", "inreji", "goonit"];
 
-console.log(numbers.concat(boooks));
+// console.log(numbers.concat(boooks));
+
+// reversing array
+
+// const numbers2 = ["1", "2", "3", "4", "5"];
+// console.log(numbers2);
+// numbers2.reverse();
+// console.log(numbers2);
+
+//? Write a JavaScript code to reverse the array colors without using the reverse method.
+
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+// let reversedColors = [];
+//* for (let i = 0; i < colors.length; i++){
+//     reversedColors.unshift(colors[i]);
+//     console.log(reversedColors);
+// }
+
+// let i = 0;
+//* while (i < colors.length) {
+//     reversedColors.unshift(colors[i]);
+//     i++;
+//     console.log(reversedColors);
+// }
+
+// ?Write a JavaScript code to get the even numbers from an array using any looping technique.
+
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+// let evenNumbers = [];
+//* for (let i = 0; i < numbers.length; i++){
+//     if (numbers[i] % 2 == 0) {
+//         evenNumbers.push(numbers[i]);
+//         console.log(evenNumbers);
+//     }
+// }
+
+// let i = 0;
+//* while (i < numbers.length) {
+//     if (numbers[i] % 2 == 0) {
+//         evenNumbers.push(numbers[i]);
+//         console.log(evenNumbers);
+//     }
+//     i++;
+// }
+
+//? Use a for...of loop to concatenate all the elements of an array into a single string. output will be 'TomTimTinTik'.
+
+// var numbers = ["Tom", "Tim", "Tin", "Tik"];
+// var result = "";
+
+// for (let number of numbers) {
+//   result += number;
+//   console.log(result);
+// }
+
+
+//? Reverse the words of a sentence. Only the position of the word will be reversed.
+
+const statement = 'I am a hard working person';
+const words = statement.split(' ');
+const reversedWords = words.reverse().join(' ');
+console.log(reversedWords);
---
+++ function.js
@@ -0,0 +1,68 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(a, b, c, d) {
+  console.log(a * b * c * d);
+}
+
+multiply(4, 5, 8, 2);
+
+// Task-2
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+function changeNumber(num) {
+  if (num % 2 !== 0) {
+    return num * 2;
+  } else {
+    return num / 2;
+  }
+}
+
+console.log(changeNumber(3)); // Output: 6
+
+// Task-3
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+function makeAvg() {
+  let sum = 0;
+  let args = Array.from(arguments);
+  let size = args.length;
+
+  for (let i = 0; i < size; i++) {
+    sum += args[i];
+  }
+
+  return sum / size;
+}
+
+console.log(makeAvg(1, 2, 3, 4, 5)); // Output: 3
+
+// Task-4
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+function countZero(binaryStr) {
+  let count = 0;
+
+  for (let i = 0; i < binaryStr.length; i++) {
+    if (binaryStr[i] === '0') {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countZero("1101011000"));
+
+// Task-5
+// Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+
+function oddEven(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(oddEven(5)); // Output: Odd
