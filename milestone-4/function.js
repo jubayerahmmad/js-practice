37909fe6ad7ffcea3fa929ecbbfa24d0ec6ac4e6
jubayerahmmad <zubayersami510@gmail.com>
2024-10-01 15:20:53 +0600

    foldering

--- milestone-4/function.js
+++ milestone-4/function.js
@@ -44,7 +44,7 @@ function countZero(binaryStr) {
   let count = 0;
 
   for (let i = 0; i < binaryStr.length; i++) {
-    if (binaryStr[i] === '0') {
+    if (binaryStr[i] === "0") {
       count++;
     }
   }
